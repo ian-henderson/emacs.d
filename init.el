e076d0d1ae18f31e41318b4cddedf656d665f0f2
Ian Henderson <iancurtish@gmail.com>
2019-04-07 13:34:25 -0500

    toggle theme mode fn

--- init.el
+++ init.el
@@ -3,75 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(fci-rule-color "#073642")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(magit-diff-use-overlays nil)
- '(package-selected-packages
-   (quote
-    (heaven-and-hell auto-package-update evil solarized-theme add-node-modules-path flycheck web-mode use-package projectile magit exec-path-from-shell autothemer auto-complete)))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#839496" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c85d17")
-     (60 . "#be730b")
-     (80 . "#b58900")
-     (100 . "#a58e00")
-     (120 . "#9d9100")
-     (140 . "#959300")
-     (160 . "#8d9600")
-     (180 . "#859900")
-     (200 . "#669b32")
-     (220 . "#579d4c")
-     (240 . "#489e65")
-     (260 . "#399f7e")
-     (280 . "#2aa198")
-     (300 . "#2898af")
-     (320 . "#2793ba")
-     (340 . "#268fc6")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -129,16 +63,8 @@
 (use-package flycheck
   :config
   (add-hook 'scss-mode-hook 'flycheck-mode)
-  (flycheck-add-mode 'sass/scss-sass-lint 'scss-mode)
-  (flycheck-add-mode 'javascript-eslint 'web-mode))
-
-;; https://github.com/valignatev/heaven-and-hell
-(use-package heaven-and-hell
-  :bind ("C-c s" . heaven-and-hell-toggle-theme)
-  :hook (after-init . heaven-and-hell-init-hook)
-  :init (setq heaven-and-hell-theme-type 'dark
-              heaven-and-hell-themes '((dark . solarized-dark)
-                                       (light . solarized-light))))
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (flycheck-add-mode 'sass/scss-sass-lint 'scss-mode))
 
 ;; https://magit.vc/manual/magit/
 (use-package magit
@@ -151,13 +77,29 @@
          ("C-c p" . projectile-command-map))
   :config (projectile-mode +1))
 
+(defun toggle-theme-mode ()
+  (interactive)
+  (cond ((equal current-theme dark-theme)
+         (load-theme light-theme)
+         (setq current-theme light-theme)
+         (message "%s" light-theme))
+        ((equal current-theme light-theme)
+         (load-theme dark-theme)
+         (setq current-theme dark-theme)
+         (message "%s" dark-theme))))
+
 ;; https://github.com/bbatsov/solarized-emacs
 (use-package solarized-theme
-  :bind ("C-c s" . 'toggle-solarized)
-  :config (load-theme 'solarized-dark)
-  :init (setq solarized-distinct-fringe-background t
-              solarized-high-contrast-mode-line t
-              solarized-use-more-italic t))
+  :bind ("C-c t" . 'toggle-theme-mode)
+  :config
+  (setq dark-theme 'solarized-dark
+        light-theme 'solarized-light
+        current-theme dark-theme)
+  (load-theme current-theme)
+  :init
+  (setq solarized-distinct-fringe-background t
+        solarized-high-contrast-mode-line t
+        solarized-use-more-italic t))
 
 (defun my-web-mode-hook ()
   "web-mode configuration."
@@ -191,10 +133,10 @@
       mac-option-modifier 'none)
 
 ;; Typeface
-(set-face-attribute 'default nil :font "PT Mono-12")
+(set-face-attribute 'default nil :font "Courier Prime Code-12")
 
 ;; Line height
-(setq-default line-spacing 0.3)
+(setq-default line-spacing 0.1)
 
 ;; Scroll bar
 (scroll-bar-mode -1)
@@ -212,8 +154,8 @@
 
 ;; Line Numbers
 (when (version<= "26.0.50" emacs-version)
-  (global-display-line-numbers-mode)
-  (setq-default display-line-numbers-type 'relative))
+  (setq-default display-line-numbers-type 'relative)
+  (global-display-line-numbers-mode))
 
 ;; Column number
 (column-number-mode)
